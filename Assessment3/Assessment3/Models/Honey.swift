4232570a4357368b3b30a4f8e2897b789c6209ce
Colton Brenneman <colton.brenneman@gmail.com>
2023-06-08 23:18:59 -0600

    finished model and modelController

--- Assessment3/Assessment3/Models/Honey.swift
+++ Assessment3/Assessment3/Models/Honey.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+class Honey: Codable {
+    
+    var choreName: String
+    var choreCount: Int
+    var isCompleted: Bool
+    let uuid: UUID
+    
+    init(choreName: String, choreCount: Int, isCompleted: Bool = false, uuid: UUID = UUID()) {
+        self.choreName = choreName
+        self.choreCount = choreCount
+        self.isCompleted = isCompleted
+        self.uuid = uuid
+    }
+} //end of class
+
+// MARK: - Extensions
+extension Honey: Equatable {
+    static func == (lhs: Honey, rhs: Honey) -> Bool {
+        return lhs.uuid == rhs.uuid
+    }
+}
